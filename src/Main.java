5eff2c367a11a435c14a15c611ff9ffe22c6daa3
Elena Tolokova <elena89-06@mail.ru>
2022-02-06 15:49:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int refill = 5000;
+        int startingScore = 10;
+        int bonus;
+
+
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        int amount = startingScore + refill + bonus;
+        System.out.println( "Итоговый счет: " + amount + " рублей" );
+    }
+        }
+
+
